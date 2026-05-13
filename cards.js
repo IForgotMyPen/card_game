651ccba575e8ab8e4a641e3faa87f7f3b85a8b37
Brendan Birschbach <brendanbirsc@gmail.com>
2026-05-12 21:51:50 -0500

    Nested some related functions under a new class Deck so that I can have different deck objects

--- cards.js
+++ cards.js
@@ -16,42 +16,60 @@ class Card {
     get image() {return this.#image}
 }
 
+class Deck {
+    #name;
+    #card_count;
+    #cards;
+
+    constructor(name, cards) {
+        this.#name = name;
+        this.#cards = cards;
+        this.#card_count = cards.length;
+    }
+
+    get name() {return this.#name;}
+    get card_count() {return this.#card_count;}
+    get cards() {return this.#cards;}
+
+    // Get a random card in the deck
+
+    getRandomCard() {
+        function getRandomInt(max) {
+            return Math.floor(Math.random() * max);
+        }
+        return this.#cards[getRandomInt(this.#card_count)];
+    }
+
+    draw() {
+        const rand_card = this.getRandomCard();
+
+        const html_card = document.getElementById('card_image');
+
+        html_card.src = `${rand_card.image}`;
+        html_card.title = `${capitalize(rand_card.rank)} of ${capitalize(rand_card.suit)}s`;
+    }
+}
+
 const suits = ['spade', 'heart', 'diamond', 'club'];
 
 const ranks = ['ace',2,3,4,5,6,7,8,9,10,'jack','queen','king'];
 
-const cards = [];
-
-// Load the deck
-
+const deck1_cards = [];
 for (const suit of suits) {
-    for(const rank of ranks) {
-        const card = new Card(suit, rank);
-        cards.push(card);
+    for (const rank of ranks) {
+        deck1_cards.push(new Card(suit, rank, `${suit}_${rank}.png`));
     }
 }
 
-// Pick random card
-
-function getRandomCard() {
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * max);
-    }
-    return cards[getRandomInt(52)];
+const deck2_cards = [];
+for (const rank of ranks) {
+    deck2_cards.push(new Card('heart', rank, `heart_${rank}.png`));
 }
 
-// Draw a card
+const deck1 = new Deck('standard deck', deck1_cards);
+const deck2 = new Deck('all-hearts deck', deck2_cards);
 
-function draw() {
-    rand_card = getRandomCard();
-
-    const html_card = document.getElementById('card_image');
-
-    html_card.src = `${rand_card.image}`;
-    html_card.title = `${capitalize(rand_card.rank)} of ${capitalize(rand_card.suit)}s`;
-}
-
-
+const current_deck = deck2;
 
 // Helper method for capitalizing
 
